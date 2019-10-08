f9f16642b7cd27523714986fe9a99362feb919f0
Tatsuya167 <clark.taka16@gmail.com>
2019-10-09 02:07:55 +0900

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Still does not use for HTML
+
+
+// var apikey;
+// var client_id;
+// var client_secret;
+// var code;
+// var scope;
+// var redirect_uri;
+// var login;
+// var scope;
+// var state;
+// var allow_signup;
